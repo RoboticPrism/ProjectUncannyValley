6bdb0450c6542e49b8f6ee02973ba879369be691
RoboticPrism <ryan92130@gmail.com>
2017-05-21 11:31:54 -0400

    Added sprite glitch.  Made husband even glitchier on day 3 (#28)

---
+++ Assets/Scripts/SpriteGlitcher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteGlitcher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Mathf.Round(Time.time) % 10 == 0)
+        {
+            if (Random.Range(0f, 1f) > 0.9f)
+            {
+                transform.localScale = new Vector3(transform.localScale.x, -1f, transform.localScale.z);
+            }
+            else
+            {
+                transform.localScale = new Vector3(transform.localScale.x, 1f, transform.localScale.z);
+            }
+        }
+	}
+}
