83f4979ce55b1bc317457bc9a01d3837d28e6740
RoboticPrism <ryan92130@gmail.com>
2017-05-21 12:10:43 -0400

    Add living room model to all scenes (#35)

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject character;
+    public float maxX = 0.0f;
+    public float minX = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(
+            Mathf.Max(Mathf.Min(character.transform.position.x, maxX), minX), 
+            transform.position.y, 
+            transform.position.z);
+	}
+}
