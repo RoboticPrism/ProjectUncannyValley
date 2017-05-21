49310879fb49db89b5adfdbc5f205120e5e683fd
RoboticPrism <ryan92130@gmail.com>
2017-05-21 13:03:38 -0400

    Animate fridge door.  Added prefab (#39)

---
+++ Assets/Scripts/RefridgeratorAnim.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RefridgeratorAnim : MonoBehaviour {
+
+    TextDisplay textDisplay;
+    public bool open = false;
+    Coroutine coroutine;
+    public float openSpeed = 0.7f;
+    public float maxOpenDegrees = 45f;
+
+    // Use this for initialization
+    void Start () {
+        textDisplay = FindObjectOfType<TextDisplay>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (open && !textDisplay.isOpen)
+        {
+            CloseFridge();
+        }
+	}
+
+    public void OpenFridge()
+    {
+        if (!open)
+        {
+            if (coroutine != null)
+            {
+                StopCoroutine(coroutine);
+            }
+            open = true;
+            coroutine = StartCoroutine("OpenAnim");
+        }
+    }
+
+    public void CloseFridge()
+    {
+        if (coroutine != null)
+        {
+            StopCoroutine(coroutine);
+        }
+        open = false;
+        coroutine = StartCoroutine("CloseAnim");
+    }
+
+    IEnumerator OpenAnim()
+    {
+        for (float i = 0; i < maxOpenDegrees; i += openSpeed)
+        {
+            transform.localEulerAngles = new Vector3(0f, i, 0f);
+            yield return null;
+        }
+    }
+
+    IEnumerator CloseAnim()
+    {
+        Debug.Log(transform.localEulerAngles.y);
+        for (float i = transform.localEulerAngles.y ; i > 0; i-= openSpeed)
+        {
+            
+            transform.localEulerAngles = new Vector3(0f, i, 0f);
+            yield return null;
+        }
+    }
+}
